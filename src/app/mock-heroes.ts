24e31f53cbbd1d5c8643450ab3ae39e812cb340d
unknown <skaloshi@gmail.com>
2018-11-26 14:36:12 +0100

    upload app

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,11 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    {id: 1, name:'Alban'},
+    {id: 2, name:'Alex'},
+    {id: 3, name:'Nico'},
+    {id: 4, name:'Momo'},
+    {id: 5, name:'Rio'},
+    {id: 6, name:'Stef'},
+    {id: 7, name:'Theo'}
+];
